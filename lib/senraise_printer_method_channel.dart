266c6991ce6a9f06e13ed5c4efc58efef412f3b9
Nicola Bellagamba <nicolabellagamba@Mac-3.homenet.telecomitalia.it>
2024-12-31 02:31:18 +0100

    added return null to all channel methods in order to avoid warnings

--- lib/senraise_printer_method_channel.dart
+++ lib/senraise_printer_method_channel.dart
@@ -20,70 +20,84 @@ class MethodChannelSenraisePrinter extends SenraisePrinterPlatform {
   @override
   Future<Void?> printEpson(Uint8List bytes) async {
     await methodChannel.invokeMethod<String>('printEpson', {"data": bytes});
+    return null;
   }
 
   @override
   Future<Void?> printText(String text) async {
     await methodChannel.invokeMethod<String>('printText', {"text": text});
+    return null;
   }
 
   @override
   Future<Void?> printPic(Uint8List pic) async {
     await methodChannel.invokeMethod<String>('printPic', {"pic": pic});
+    return null;
   }
 
   @override
   Future<Void?> printBarCode(String data, int symbology, int height, int width) async {
     await methodChannel.invokeMethod<String>('printPic', {"data": data, "symbology": symbology, "height": height, "width": width});
+    return null;
   }
 
   @override
   Future<Void?> printQRCode(String data, int modulesize, int errorlevel) async {
     await methodChannel.invokeMethod<String>('printQRCode', {"data": data, "modulesize": modulesize, "errorlevel": errorlevel});
+    return null;
   }
 
   @override
   Future<Void?> setAlignment(int alignment) async {
     await methodChannel.invokeMethod<String>('setAlignment', {"alignment": alignment});
+    return null;
   }
 
   @override
   Future<Void?> setTextSize(double textSize) async {
     await methodChannel.invokeMethod<String>('setTextSize', {"textSize": textSize});
+    return null;
   }
 
   @override
   Future<Void?> nextLine(int line) async {
     await methodChannel.invokeMethod<String>('nextLine', {"line": line});
+    return null;
   }
 
   @override
   Future<Void?> setTextBold(bool bold) async {
     await methodChannel.invokeMethod<String>('setTextBold', {"bold": bold});
+    return null;
   }
 
   @override
   Future<Void?> setDark(int value) async {
     await methodChannel.invokeMethod<String>('setDark', {"value": value});
+    return null;
   }
 
   @override
   Future<Void?> setLineHeight(double lineHeight) async {
     await methodChannel.invokeMethod<String>('setLineHeight', {"lineHeight": lineHeight});
+    return null;
   }
 
   @override
   Future<Void?> setTextDoubleWidth(bool enable) async {
     await methodChannel.invokeMethod<String>('setTextDoubleWidth', {"enable": enable});
+    return null;
   }
 
   @override
   Future<Void?> setTextDoubleHeight(bool enable) async {
     await methodChannel.invokeMethod<String>('setTextDoubleHeight', {"enable": enable});
+    return null;
   }
 
   @override
   Future<Void?> setCode(String code) async {
     await methodChannel.invokeMethod<String>('setCode', {"code": code});
+    return null;
   }
 }
